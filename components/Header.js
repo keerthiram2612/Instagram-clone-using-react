8b71c6b5eeab2b9a75ad0b248d18a9ae39beba94
keerthiram2612 <keerthiram2612@gmail.com>
2023-02-14 21:44:50 +0530

    add search bar to the header component

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,15 @@
 import Image from 'next/image';
 import react from 'react';
-
+import { SearchIcon } from '@heroicons/react/outline';
 export default function Header(){
     return(
-        <div>
-            
-            {/*Left*/}
+        
+          
             
             <div className="flex items-center justify-between max-w-6xl">
+                  
+            {/*Left*/}
+            
                 <div className=" cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
                     <Image
                      src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1024px-Instagram_logo.svg.png"
@@ -25,13 +27,23 @@ export default function Header(){
 
                     />
                 </div>
+                    {/*Middle*/}
+                <div className="relative mt-1">
+                    <div className="absolute top-2 left-2">
+                        <SearchIcon className="h-5 text-gray-500"/>
+                    </div>
+                    <input type="text" placeholder="Search" className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"/>
+                </div>
+
+                     {/*Right*/}
+
                <h1>Right sides</h1>
             </div>
 
-            {/*Middle*/}
+        
 
-            {/*Right*/}
+           
 
-        </div>
-    )
+       
+    );
 }

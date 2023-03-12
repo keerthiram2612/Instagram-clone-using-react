baafadd7dc800ba7e4d6cd6a61193c9ac16d2b48
keerthiram2612 <keerthiram2612@gmail.com>
2023-03-12 19:03:58 +0530

    populate fake data using minifaker and create feed and story components

---
+++ components/Feed.js
@@ -0,0 +1,21 @@
+import Stories from "./Stories";
+
+
+export default function Feed() {
+  return (
+    <main>
+       <section>
+        {/*Stories*/}
+         <Stories/>
+        {/*Posts*/}
+
+       </section>
+
+       <section>
+       {/*Mini Profile*/}
+
+       {/*Suggestion*/}
+       </section>
+    </main>
+  )
+}

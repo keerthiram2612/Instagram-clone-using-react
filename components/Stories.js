2e301f23ce45227f58316d20a3c692168c422b81
keerthiram2612 <keerthiram2612@gmail.com>
2023-03-24 06:32:45 +0530

    not functioing of the scrollbar in the component of stories section

--- components/Stories.js
+++ components/Stories.js
@@ -19,9 +19,9 @@ export default function Stories() {
 
     },[]);
   return (
-    <div>
+    <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 border overflow-x-scroll rounded-sm scrollbar-none">
        {storyUsers.map(user=>(
-        <Story key={user.id} username={user.username}img={user.img}/>
+        <Story key={user.id} username={user.username}img={user.img} />
        ))}
     </div>
   )

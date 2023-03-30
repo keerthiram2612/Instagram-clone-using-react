9b91f842a62c1873854491d65efc66a29dfa962b
keerthiram2612 <keerthiram2612@gmail.com>
2023-03-30 21:54:51 +0530

    create the header and image sections of the post component

--- components/Posts.js
+++ components/Posts.js
@@ -7,18 +7,18 @@ export default function Posts() {
             id:"1",
             username: "Keerthi__dog__lover",
             userImg : "https://png.pngtree.com/png-clipart/20200720/original/pngtree-luxury-letter-k-logo-design-png-image_4655196.jpg",
-            img:"https://images.unsplash.com/photo-1679858511194-8ab47c0449be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=700&q=60",
+            img:"https://images.unsplash.com/photo-1680120603076-c086ec523217?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
             caption: "Nice picture"
         },
         {
             id:"2",
             username: "jeni__jerry2611",
             userImg : "https://png.pngtree.com/png-clipart/20200720/original/pngtree-luxury-letter-k-logo-design-png-image_4655196.jpg",
-            img:"https://images.unsplash.com/photo-1679882069275-1c43fefbe81a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=700&q=60",
+            img:"https://images.unsplash.com/photo-1680017534357-2edd2e803341?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyOHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
             caption: "New picture from my city"
         }
     ]
-  return ( <div>
+  return <div>
     {posts.map(post=>(
        <Post
        key={post.id}
@@ -27,9 +27,11 @@ export default function Posts() {
        userImg={post.userImg}
        img={post.img}
        caption={post.caption}
+       
        />
     ))}
        
     </div>
-  )
+  
+  
 }
